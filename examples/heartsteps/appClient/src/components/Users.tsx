4eac2dd65d7a257bdba4754af8b91340ff2b725d
patrick9958 <patrick.neggie@gmail.com>
2023-03-09 00:19:15 -0500

    jwt pipeline behaves correctly

--- examples/heartsteps/appClient/src/components/Users.tsx
+++ examples/heartsteps/appClient/src/components/Users.tsx
@@ -10,6 +10,28 @@ const Users = () => {
   const axiosPrivate = useAxiosPrivate();
   const navigation = useNavigation();
 
+  const updateUsers = async () => {
+    console.log('RUNNING GET USERS HOOK');
+    try {
+      const response = await axiosPrivate.get('/users');
+      // TODO: replace 'any' with user TS type
+      const userNames = response.data.map((user: any) => user.username);
+      // TODO: SECURITY - DELETE LOG IN PRODUCTION
+      // console.log(response.data);
+      setUsers(userNames);
+    } catch (err) {
+      // FIXME: TEST REFRESH TOKEN TIMEOUT BEHAVIOR
+      console.log(err);
+      console.log('ACCESS TOKEN EXPIRED WHILE FETCHING USERS');
+      /* 
+      whenever a user's refresh token expires
+      after they re-login, user is sent back to the last page they were on
+      i.e. user sent back to /participant/john/name page instead of /home page
+      */
+      navigation.navigate('Login');
+    }
+  };
+
   useEffect(() => {
     let isMounted = true;
     // controller is to cancel request if component unmounts
@@ -53,6 +75,7 @@ const Users = () => {
   return (
     <View>
       <Text>Users List</Text>
+      <Button title="Update Users" onPress={updateUsers} />
       {users?.length ? (
         <ScrollView>
           {/* TODO: replace key with uuid */}
